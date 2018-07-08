43a271ba9d07d36dc96253b56cfba8c46458b118
Brock Lanoza <brock@digit.co>
2018-07-07 19:39:16 -0700

    Add remaining endpoints

--- index.js
+++ index.js
@@ -4,27 +4,80 @@ const db = require("./db");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
+app.delete("/documentation/:pageId/notes/:noteId", (req, res) => {
+  const { pageId, noteId } = req.params;
+  const query = "DELETE FROM notes WHERE page_id = $1 AND id = $2";
+  const values = [pageId, noteId];
 
-app.get("/documentation/:pageId/notes", function(req, res) {
-  const query = "SELECT * FROM notes WHERE page_id = $1";
-  const page_id = req.params.pageId;
-  const values = [page_id];
-
-  if (!page_id) {
-    return res.status(400).json({ error: "Page Id required in the request" });
-  }
   db.query(query, values, (err, result) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    res.json({ data: result.rows });
+    return res.status(204).json({});
+  });
+});
+
+app.put("/documentation/:pageId/notes/:noteId", (req, res) => {
+  const { pageElement, noteBody } = req.body;
+  if (!pageElement || !noteBody) {
+    return res.status(400).json({ error: "Missing required parameters pageElement and noteBody" });
+  }
+  const query = "UPDATE notes SET page_id = $1, page_element = $2, note_body = $3";
+  const pageId = req.params.pageId;
+  const values = [pageId, pageElement, noteBody];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    return res.json({ page_id: pageId, page_element: pageElement, note_body: noteBody });
+  });
+});
+
+app.post("/documentation/:pageId/notes", (req, res) => {
+  const { pageElement, noteBody } = req.body;
+  if (!pageElement || !noteBody) {
+    return res.status(400).json({ error: "Missing required parameters pageElement and noteBody" });
+  }
+  const query = "INSERT INTO notes (page_id, page_element, note_body) VALUES($1, $2, $3)";
+  const pageId = req.params.pageId;
+  const values = [pageId, pageElement, noteBody];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    return res.json({ page_id: pageId, page_element: pageElement, note_body: noteBody });
+  });
+});
+
+app.get("/documentation/:pageId/notes", (req, res) => {
+  const query = "SELECT * FROM notes WHERE page_id = $1";
+  const pageId = req.params.pageId;
+  const values = [pageId];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    return res.json(result.rows);
+  });
+});
+
+app.get("/documentation/:pageId/notes/:noteId", (req, res) => {
+  const query = "SELECT * FROM notes WHERE id = $1";
+  const pageId = req.params.pageId;
+  const values = [pageId];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    const note = result.rows[0];
+    return res.json({ note });
   });
 });
 
