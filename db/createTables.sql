f872d66909d7006494df094d459e9020ee21666a
Brock Lanoza <brock@digit.co>
2018-07-08 19:42:12 -0700

    Set default on page_element to null

--- db/createTables.sql
+++ db/createTables.sql
@@ -1,6 +1,6 @@
 create table if not exists notes (
     id serial primary key,
     page_id text not null,
-    page_element text not null,
+    page_element text default null,
     note_body varchar(256) not null
 );
