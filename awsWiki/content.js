6f9c43326112159c133519172faf3e7161f530b7
Brock Lanoza <brock@digit.co>
2018-07-08 19:43:59 -0700

    Lint code

--- awsWiki/content.js
+++ awsWiki/content.js
@@ -16,28 +16,28 @@ function addNoteToElement(el) {
 window.onload = function () {
   const pageId = 2;// window.location.pathname;
   fetchNotes(pageId)
-      .then((pageNotes) => {
-          console.log(pageNotes, 'NOTES');
-          // Page notes will be an array. Loop over each note and place a marker on the page
-          pageNotes.forEach((note) => {
-              let noteElement;
-              // Highlighted element didn't have a class/id, append to document title
-              if (!note.pageElement) {
-                  noteElement = document
-                      .getElementsByTagName("h1")[0]
-                      .insertAdjacentHTML(
-                          "beforebegin",
-                          '<div id="andrea-test">NOTES BRAH!!!!!</div>'
-                      );
-              } else {
-                  noteElement = document.querySelector(note.pageElement);
-              }
-              addNoteToElement(noteElement);
-          });
-      })
-      .catch((err) => {
-          console.log(err, 'ERR');
-          // Do something here to let the user know the app failed to fetch the page notes
+    .then((pageNotes) => {
+      console.log(pageNotes, 'NOTES');
+      // Page notes will be an array. Loop over each note and place a marker on the page
+      pageNotes.forEach((note) => {
+        let noteElement;
+        // Highlighted element didn't have a class/id, append to document title
+        if (!note.pageElement) {
+            noteElement = document
+                .getElementsByTagName("h1")[0]
+                .insertAdjacentHTML(
+                    "beforebegin",
+                    '<div id="andrea-test">NOTES BRAH!!!!!</div>'
+                );
+        } else {
+          noteElement = document.querySelector(note.pageElement);
+        }
+        addNoteToElement(noteElement);
       });
+    })
+    .catch((err) => {
+      console.log(err, 'ERR');
+      // Do something here to let the user know the app failed to fetch the page notes
+    });
 };   
-};
+
