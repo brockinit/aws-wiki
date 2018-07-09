41ee5b76aec231b1ccc76c6298d7788c06cb4f4a
Brock Lanoza <brock@digit.co>
2018-07-08 19:46:15 -0700

    Add prettier

--- awsWiki/content.js
+++ awsWiki/content.js
@@ -1,34 +1,33 @@
 function fetchNotes(pageId, callback) {
   const getNotesUrl = `https://xc56ahkj9l.execute-api.us-west-2.amazonaws.com/development/documentation/${pageId}/notes`;
   fetch(getNotesUrl)
-      .then((result) => {
-          return result.json();
-      });
+    .then((result) => {
+      return result.json();
+    });
 }
 
 function addNoteToElement(el) {
   el.insertAdjacentHTML(
-      "beforebegin",
-      '<div id="andrea-test">NOTES BRAH!!!!!</div>'
+    "beforebegin",
+    '<div id="andrea-test">NOTES BRAH!!!!!</div>'
   );
 }
 
 window.onload = function () {
-  const pageId = 2;// window.location.pathname;
+  const pageId = window.location.pathname;
   fetchNotes(pageId)
     .then((pageNotes) => {
-      console.log(pageNotes, 'NOTES');
       // Page notes will be an array. Loop over each note and place a marker on the page
       pageNotes.forEach((note) => {
         let noteElement;
         // Highlighted element didn't have a class/id, append to document title
         if (!note.pageElement) {
-            noteElement = document
-                .getElementsByTagName("h1")[0]
-                .insertAdjacentHTML(
-                    "beforebegin",
-                    '<div id="andrea-test">NOTES BRAH!!!!!</div>'
-                );
+          noteElement = document
+            .getElementsByTagName("h1")[0]
+            .insertAdjacentHTML(
+              "beforebegin",
+              '<div id="andrea-test">NOTES BRAH!!!!!</div>'
+            );
         } else {
           noteElement = document.querySelector(note.pageElement);
         }
@@ -36,8 +35,7 @@ window.onload = function () {
       });
     })
     .catch((err) => {
-      console.log(err, 'ERR');
       // Do something here to let the user know the app failed to fetch the page notes
     });
-};   
+};
 
